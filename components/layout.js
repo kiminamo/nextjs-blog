10004833916719e76c431b2abb11dcb6fdbd5176
Kimberly Manaloto <140568363+kiminamo@users.noreply.github.com>
2024-08-27 14:02:34 +0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Kimberly G. Manaloto';
+const name = 'Kimberly Manaloto';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
