94e0903d4e914cd2d3d339e0f8f9c4b6d86ca5a9
Kimberly Manaloto <140568363+kiminamo@users.noreply.github.com>
2024-08-27 14:11:29 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,13 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <hr />
 
-    <p align="center">
-  My name is Kimberly Guevarra Manaloto, and I am 20 years old. I have a passion for editing and photography. <br />
-  I chose to study IT because technology has always fascinated me, and I want to create things <br />
-  that make people's lives easier. <br /> After graduation, I hope to become <br />
-  a software developer and work on projects that help businesses or individuals. In this subject, I expect to gain a deeper <br />
-  understanding of programming languages and tools for building better software. I would also love <br />
-  to learn more about front-end design, user experience, and cloud technologies.
+    <p style="text-align: justify; line-height: 1.6;">
+  My name is Kimberly Guevarra Manaloto, and I am 20 years old. I have a passion for editing and photography. 
+  I chose to study IT because technology has always fascinated me, and I want to create things that make people's lives easier. 
+  After graduation, I hope to become a software developer and work on projects that help businesses or individuals. 
+  In this subject, I expect to gain a deeper understanding of programming languages and tools for building better software. 
+  I would also love to learn more about front-end design, user experience, and cloud technologies.
 </p>
-       
       
        
       <hr />
