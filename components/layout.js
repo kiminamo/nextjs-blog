95b839786ae7eb7893582b80ca794c003519c826
Kimberly Manaloto <140568363+kiminamo@users.noreply.github.com>
2024-09-10 13:44:19 +0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Kimberly Manaloto';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'Kimberly G. Manaloto';
+export const siteTitle = 'No Talent Required';
 
 export default function Layout({ children, home }) {
   return (
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/kim.png"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +43,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/kim.png"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
